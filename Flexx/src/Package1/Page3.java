4fca96b899aa433ed01d7a0e30ab38163c9eaa06
TC6IDM <TCAND6@GMAIL.COM>
2023-02-15 00:00:33 -0500

    Update Page3.java
    Set the correct position for the buttons, new exercise button moves down when pressed, new classes: Workout and Set added, just boiler plate for now, more to come

--- Flexx/src/Package1/Page3.java
+++ Flexx/src/Package1/Page3.java
@@ -9,12 +9,24 @@ import javax.swing.SwingConstants;
 import java.awt.Font;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
 import java.awt.event.ActionEvent;
 
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 
 /* Andrew Tissi */
+
+class Workout{
+	public String name; 
+	public ArrayList<Set> sets = new ArrayList<Set>();
+}
+
+class Set extends Workout{
+	private int reps;
+	private int weight;	
+}
+
 public class Page3 {
 
 	public JFrame frame;
@@ -47,13 +59,19 @@ public class Page3 {
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
+		int Frame_Left = 100;
+		int Frame_Width = 450;
+		int Frame_Top = 100;
+		int Frame_Height = 300;
+		int Frame_Center = Frame_Width/2;
+		
 		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
+		frame.setBounds(Frame_Left, Frame_Top, Frame_Width, Frame_Height);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
 		
 		JLabel lblNewLabel = new JLabel("PAGE 3");
-		lblNewLabel.setBounds(132, 10, 172, 35);
+		lblNewLabel.setBounds(Frame_Center-50, 10, 100, 35);
 		lblNewLabel.setFont(new Font("Berlin Sans FB", Font.PLAIN, 31));
 		frame.getContentPane().add(lblNewLabel);
 		
@@ -65,8 +83,31 @@ public class Page3 {
 				frame.setVisible(false);
 			}
 		});
-		btnNewButton.setBounds(10, 10, 77, 35);
+		btnNewButton.setBounds(10, 10, 75, 35);
 		frame.getContentPane().add(btnNewButton);
+		
+		JButton btnNewButton2 = new JButton("Done");
+		btnNewButton2.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				Home home = new Home();
+				home.frame.setVisible(true);
+				frame.setVisible(false);
+			}
+		});
+		btnNewButton2.setBounds(Frame_Width-100, 10, 75, 35);
+		frame.getContentPane().add(btnNewButton2);
+		
+		int btnNewButton_1_width = 100;
+		int btnNewButton_1_height = 35;	
+		int btnNewButton_1_distanceFromTop = 50;
+		JButton btnNewButton_1 = new JButton("New button");
+		btnNewButton_1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				btnNewButton_1.setLocation(btnNewButton_1.getX(), btnNewButton_1.getY()+75);
+			}
+		});
+		btnNewButton_1.setBounds(Frame_Center-btnNewButton_1_width/2, btnNewButton_1_distanceFromTop, btnNewButton_1_width, btnNewButton_1_height);
+		frame.getContentPane().add(btnNewButton_1);
 	}
 
 }
