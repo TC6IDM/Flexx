f5b5c64000d27cf9f1ea74b1a953b917a80d0ae2
AvaYunus <avayunus02@gmail.com>
2023-02-16 15:28:56 -0500

    Updated Home/Login
    Created a login system using a users.txt file.
    Sign up, back and login buttons implemented with full functionality.
    users.txt will be automatically created with sign up.

--- Flexx/src/Package1/Login.java
+++ Flexx/src/Package1/Login.java
@@ -8,6 +8,13 @@ import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Map;
 import java.awt.event.ActionEvent;
 import javax.swing.JPasswordField;
 import javax.swing.JCheckBox;
@@ -15,7 +22,7 @@ import javax.swing.JCheckBox;
 import java.awt.Font;
 import java.awt.Window;
 
-public class Login {
+public class Login implements ActionListener{
 
 	public JFrame frame;
 	private JTextField usernameField;
@@ -41,8 +48,10 @@ public class Login {
 	 * Create the application.
 	 */
 	public Login() {
-		initialize();
+		initialize();	
 	}
+
+	
 
 	/**
 	 * Initialize the contents of the frame.
@@ -73,27 +82,95 @@ public class Login {
 		lblUsername.setBounds(51, 62, 140, 19);
 		frame.getContentPane().add(lblUsername);
 		
+		JButton btnNewBackButton = new JButton("Back");
+		btnNewBackButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				Home home = new Home();
+				home.frame.setVisible(true);
+				frame.setVisible(false);
+			}
+		});
+		btnNewBackButton.setBounds(10, 10, 77, 35);
+		frame.getContentPane().add(btnNewBackButton);
+		
 		JButton btnNewButton = new JButton("Login");
 		btnNewButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				String username = usernameField.getText();
 				String password = passwordField.getText();
+				boolean found = false;
 				
-				if (username.equals("e") && password.equals("e")) {
-					JOptionPane.showMessageDialog(frame, "Welcome");
-				
-					Home home = new Home();
+		        try {
+		            BufferedReader reader = new BufferedReader(new FileReader("users.txt"));
+		            String line;
+		            while ((line = reader.readLine()) != null) {
+		                String[] parts = line.split(",");
+		                if (parts.length == 2 && parts[0].equals(username) && parts[1].equals(password)) {
+		                    found = true;
+		                    break;
+		                }
+		            }
+		            reader.close();
+		        } catch (IOException ex) {
+		            ex.printStackTrace();
+		        }
+		        
+		        if (found) {
+		        	JOptionPane.showMessageDialog(frame, "Welcome");
+		        	Home home = new Home();
 					home.frame.setVisible(true);
 					frame.setVisible(false);
 					
-				}
-				else {
+		        } else {
 					JOptionPane.showMessageDialog(frame, "Invalid Login");
 				}
 				
  			}
 		});
-		btnNewButton.setBounds(172, 189, 85, 21);
+		
+		btnNewButton.setBounds(176, 189, 85, 21);
 		frame.getContentPane().add(btnNewButton);
+		
+		JButton btnClearButton = new JButton("Clear");
+		btnClearButton.setBounds(47, 185, 117, 29);
+		frame.getContentPane().add(btnClearButton);
+		
+		JButton btnNewButton_1 = new JButton("Sign up");
+		btnNewButton_1.setBounds(273, 185, 117, 29);
+		frame.getContentPane().add(btnNewButton_1);
+		btnNewButton_1.addActionListener(new ActionListener() {
+		    public void actionPerformed(ActionEvent e) {
+		        JTextField usernameField = new JTextField();
+		        JTextField passwordField = new JPasswordField();
+		        Object[] fields = {
+		            "Username:", usernameField,
+		            "Password:", passwordField
+		        };
+		        int option = JOptionPane.showConfirmDialog(frame, fields, "Sign up", JOptionPane.OK_CANCEL_OPTION);
+		        if (option == JOptionPane.OK_OPTION) {
+		            String username = usernameField.getText();
+		            String password = passwordField.getText();
+		            // save the username and password to a database or file
+		            try {
+		            	File file = new File("users.txt");
+		            	if(!file.exists()) {
+		            		file.createNewFile();
+		            	}
+		                BufferedWriter writer = new BufferedWriter(new FileWriter(file, true));
+		                writer.write(username + "," + password + "\n");
+		                writer.close();
+		            JOptionPane.showMessageDialog(frame, "You have successfully signed up!");
+		            } catch (IOException ex) {
+		                ex.printStackTrace();
+		            }
+		        }
+		    }
+		});
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
 	}
 }
