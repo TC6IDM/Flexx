12fdb8fc7b6086bf789266a0fe3163e304d3c989
TC6IDM <TCAND6@GMAIL.COM>
2023-03-01 12:15:56 -0500

    foundation for mySQL
    added files to start with our database

---
+++ Flexx/src/Package1/JDBC.java
@@ -0,0 +1,32 @@
+package Package1;
+import java.sql.*;
+public class JDBC{
+	public static void main(String[] args) {
+//		String url = "jdbc:mysql://localhost:3306/2311Labtest3" ;
+//		String user = "root" ;
+//		String password = "MyN3wP4ssw0rd" ; // replace ... with your password
+//		String query = "SELECT * FROM Property;" ; // replace ... with the correct query
+//		
+//		try {
+//			// create connection
+//			Connection con = DriverManager.getConnection (url,user,password);
+//			
+//			 // create statement
+//			Statement statement = con.createStatement();
+//			
+//			 // generate result set
+//			ResultSet result = statement.executeQuery(query);
+//			
+//			 // loop through the result set
+//			while (result.next()) {
+//				String data = "";
+//				for (int i = 1; i < 4; i++) {
+//					data += result.getString(i)+ " ";
+//				}
+//				System.out.println(data);
+//			}
+//		} catch (SQLException e) {
+//			e.printStackTrace();
+//		}
+	}
+}
