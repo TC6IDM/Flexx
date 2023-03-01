4958641d385f8c648d9e078e873a55262dd6fde9
TC6IDM <TCAND6@GMAIL.COM>
2023-03-01 12:33:45 -0500

    creating first database and table

--- Flexx/src/Package1/Flexx.java
+++ Flexx/src/Package1/Flexx.java
@@ -2,6 +2,8 @@ package Package1;
 
 public class Flexx{
 	public static void main(String[] args) {
+		JDBC.createDB();
+		JDBC.createTables();
 		Login login = new Login();
 		login.frame.setVisible(true);
 	}
--- Flexx/src/Package1/JDBC.java
+++ Flexx/src/Package1/JDBC.java
@@ -1,32 +1,50 @@
 package Package1;
 import java.sql.*;
 public class JDBC{
-	public static void main(String[] args) {
-//		String url = "jdbc:mysql://localhost:3306/2311Labtest3" ;
-//		String user = "root" ;
-//		String password = "MyN3wP4ssw0rd" ; // replace ... with your password
-//		String query = "SELECT * FROM Property;" ; // replace ... with the correct query
-//		
-//		try {
-//			// create connection
-//			Connection con = DriverManager.getConnection (url,user,password);
-//			
-//			 // create statement
-//			Statement statement = con.createStatement();
-//			
-//			 // generate result set
-//			ResultSet result = statement.executeQuery(query);
-//			
-//			 // loop through the result set
-//			while (result.next()) {
-//				String data = "";
-//				for (int i = 1; i < 4; i++) {
-//					data += result.getString(i)+ " ";
-//				}
-//				System.out.println(data);
-//			}
-//		} catch (SQLException e) {
+	private static String password = "MyN3wP4ssw0rd";// replace ... with your password
+	public static void main(String[] args) {}
+	public static void createDB() {
+		String url = "jdbc:mysql://localhost:3306/" ;
+		String user = "root" ;
+		String query = "CREATE database Flexx" ;
+		
+		try {
+			// create connection
+			Connection con = DriverManager.getConnection (url,user,password);
+			
+			 // create statement
+			Statement statement = con.createStatement();
+			
+			 // generate result set
+			statement.execute(query);
+			
+
+		} catch (SQLException e) {
 //			e.printStackTrace();
-//		}
+		}
+		System.out.println("database created");
+	}
+	public static void createTables() {
+		String url = "jdbc:mysql://localhost:3306/Flexx" ;
+		String user = "root" ;
+		
+		
+		String query = "CREATE TABLE userInfo( username VARCHAR(255) NOT NULL, password VARCHAR(255) NOT NULL);" ;
+		
+		try {
+			// create connection
+			Connection con = DriverManager.getConnection (url,user,password);
+			
+			 // create statement
+			Statement statement = con.createStatement();
+			
+			 // generate result set
+			statement.execute(query);
+			
+
+		} catch (SQLException e) {
+//			e.printStackTrace();
+		}
+		System.out.println("table 1 created");
 	}
 }
