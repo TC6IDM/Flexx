7e078fed99507299dad30897106234a5886c096f
TC6IDM <TCAND6@GMAIL.COM>
2023-03-09 15:52:34 -0500

    cleaning up code
    Created a method to house some code pertaining to checking if every text field has a  valid answer
    
    also added some comments

--- Flexx/src/Package1/Page3.java
+++ Flexx/src/Package1/Page3.java
@@ -23,7 +23,6 @@ import javax.swing.JScrollBar;
 /* Andrew Tissi */
 
 public class Page3 {
-	public int height = 0;
 	public JFrame frame;
 	public ArrayList<Exercise> exercises = new ArrayList<Exercise>();
 	private JTextField textField;
@@ -60,64 +59,109 @@ public class Page3 {
 	 * Initialize the contents of the frame.
 	 */
 	
-	public boolean move(int move) {
-		int CUTOFF = 50;
-		boolean canMove = true;
-		if (exercises.size()==0) return false;
-		if (exercises.get(0).newExerciseButton.getY()+move < CUTOFF) {move = -1*(exercises.get(0).newExerciseButton.getY() - CUTOFF); canMove = false;}
-		if (exercises.get(0).nameField.getY()+move > CUTOFF) {move = CUTOFF - exercises.get(0).nameField.getY(); canMove = false;}
-		height+= move;
-		exercises.get(0).newExerciseButton.setLocation(exercises.get(0).newExerciseButton.getX(),exercises.get(0).newExerciseButton.getY()+move);
+	public String isValid() {
+		String invalidInfo ="\n";
+		if (exercises.size()==0) return "|No Exercises|";
 		for (int i=0;i<exercises.size();i++) {
 			Exercise currentExercise = exercises.get(i);
+			String exerciseName = currentExercise.nameField.getText();
+			if (exerciseName.equals("")) invalidInfo = invalidInfo+ "|Exercise #"+currentExercise.exerciseNumber+" Name Invalid|\n";
+			for (int k=0;k<currentExercise.sets.size();k++) {
+				Set currentSet = currentExercise.sets.get(k);
+				String reps = currentSet.repsField.getText();
+				String weight = currentSet.weightField.getText();
+				if (reps.equals("")) invalidInfo = invalidInfo+ "|Exercise #"+currentExercise.exerciseNumber+" Set #"+currentSet.setNumber+" Reps Invalid|\n";
+				if (weight.equals(""))invalidInfo = invalidInfo+ "|Exercise #"+currentExercise.exerciseNumber+" Set #"+currentSet.setNumber+" Weight Invalid|\n";
+				
+				try {
+					Integer.parseInt(reps);
+			    } catch (NumberFormatException nfe) {
+			    	if (!reps.equals("")) invalidInfo = invalidInfo+ "|Exercise #"+currentExercise.exerciseNumber+" Set #"+currentSet.setNumber+" Reps must be an Integer|\n";
+			    }
+				
+				try {
+			        Double.parseDouble(weight);
+			    } catch (NumberFormatException nfe) {
+			    	if (!weight.equals(""))invalidInfo = invalidInfo+ "|Exercise #"+currentExercise.exerciseNumber+" Set #"+currentSet.setNumber+" Weight must be a double|\n";
+			    }
+			}
+		}
+		return invalidInfo;
+	}
+	//moves all elements of the work-outs up or down by x 
+	public boolean move(int move) {
+		int CUTOFF = 50; //height cutoff, the exercise button can not go above this point, and the first exercise can not be below this point
+		boolean canMove = true;//keeps track if the exercises can move
+		if (exercises.size()==0) return false; //no exercises and therefore return false, nothing will move
+		if (exercises.get(0).newExerciseButton.getY()+move < CUTOFF) {move = -1*(exercises.get(0).newExerciseButton.getY() - CUTOFF); canMove = false;} //if the next move will put the exercise button above the cutoff, then only move by however much can keep it right at the cutoff
+		if (exercises.get(0).nameField.getY()+move > CUTOFF) {move = CUTOFF - exercises.get(0).nameField.getY(); canMove = false;}//if the next move will put the first exercise below the cutoff, then only move by however much can keep it right at the cutoff
+		exercises.get(0).newExerciseButton.setLocation(exercises.get(0).newExerciseButton.getX(),exercises.get(0).newExerciseButton.getY()+move); //moves the new exercise button
+		
+		for (int i=0;i<exercises.size();i++) { //loops through all exercises
+			Exercise currentExercise = exercises.get(i); //finds the current exercise
+			//moves the three elements of exercise
 			currentExercise.nameLabel.setLocation(currentExercise.nameLabel.getX(),currentExercise.nameLabel.getY()+move);
 			currentExercise.nameField.setLocation(currentExercise.nameField.getX(),currentExercise.nameField.getY()+move);
-			currentExercise.addSetButton.setLocation(currentExercise.addSetButton.getX(),currentExercise.addSetButton.getY()+move);
-			if (currentExercise.nameLabel.getY()<CUTOFF) currentExercise.nameLabel.setVisible(false); else currentExercise.nameLabel.setVisible(true);
+			currentExercise.addSetButton.setLocation(currentExercise.addSetButton.getX(),currentExercise.addSetButton.getY()+move); 
+			
+			//for each element of exercise, it will turn invisible if it is beyond the cutoff and it will be visible if not beyond the cutoff
+			if (currentExercise.nameLabel.getY()<CUTOFF) currentExercise.nameLabel.setVisible(false); else currentExercise.nameLabel.setVisible(true); 
 			if (currentExercise.nameField.getY()<CUTOFF) currentExercise.nameField.setVisible(false); else currentExercise.nameField.setVisible(true);
 			if (currentExercise.addSetButton.getY()<CUTOFF) currentExercise.addSetButton.setVisible(false); else currentExercise.addSetButton.setVisible(true);
 			
-			for (int k=0;k<currentExercise.sets.size();k++) {
-				Set currentSet = currentExercise.sets.get(k);
-				currentSet.setCount.setLocation(currentSet.setCount.getX(),currentSet.setCount.getY()+move);
-				currentSet.repsLabel.setLocation(currentSet.repsLabel.getX(),currentSet.repsLabel.getY()+move);
+			for (int k=0;k<currentExercise.sets.size();k++) { //loops through all sets
+				Set currentSet = currentExercise.sets.get(k); //finds the current set
+				//moves the five elements of exercise
+				currentSet.setCount.setLocation(currentSet.setCount.getX(),currentSet.setCount.getY()+move); 
+				currentSet.repsLabel.setLocation(currentSet.repsLabel.getX(),currentSet.repsLabel.getY()+move); 
 				currentSet.repsField.setLocation(currentSet.repsField.getX(),currentSet.repsField.getY()+move);
 				currentSet.weightLabel.setLocation(currentSet.weightLabel.getX(),currentSet.weightLabel.getY()+move);
 				currentSet.weightField.setLocation(currentSet.weightField.getX(),currentSet.weightField.getY()+move);
+				
+				//for each element of the set, it will turn invisible if it is beyond the cutoff and it will be visible if not beyond the cutoff
 				if (currentSet.setCount.getY()<CUTOFF) currentSet.setCount.setVisible(false); else currentSet.setCount.setVisible(true);
 				if (currentSet.repsLabel.getY()<CUTOFF) currentSet.repsLabel.setVisible(false); else currentSet.repsLabel.setVisible(true);
 				if (currentSet.repsField.getY()<CUTOFF) currentSet.repsField.setVisible(false); else currentSet.repsField.setVisible(true);
 				if (currentSet.weightLabel.getY()<CUTOFF) currentSet.weightLabel.setVisible(false); else currentSet.weightLabel.setVisible(true);
 				if (currentSet.weightField.getY()<CUTOFF) currentSet.weightField.setVisible(false); else currentSet.weightField.setVisible(true);
+				
+				//updates the frame
 				frame.validate();
 				frame.repaint();
 			}
 		}
-		return canMove;
+		return canMove; //returns whether the items can move
 		
 	}
 	private void initialize() {
+		//sets the bounds of the frame
 		int Frame_Left = 100;
 		int Frame_Width = 450;
 		int Frame_Top = 100;
 		int Frame_Height = 600;
 		int Frame_Center = Frame_Width/2;
 		
+		//creates the frame
 		frame = new JFrame();
 		frame.setBounds(Frame_Left, Frame_Top, Frame_Width, Frame_Height);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
+		
+		//creates the Track work-out Title
 		int pageLabel_width = 200;
 		JLabel pageLabel = new JLabel("Track Workout");
 		pageLabel.setBounds(Frame_Center-pageLabel_width/2, 10, pageLabel_width, 35);
 		pageLabel.setFont(new Font("Berlin Sans FB", Font.PLAIN, 31));
 		frame.getContentPane().add(pageLabel);
 		
+		//creates the back button
 		JButton backButton = new JButton("Back");
 		backButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-		        int option = JOptionPane.showConfirmDialog(frame, "Are you sure you would like to go back? This Current Workout will be Lost...", "Cancel?", JOptionPane.YES_NO_OPTION);
-		        if (option == JOptionPane.YES_OPTION) {
+		        //creates a pop up confirmation for going back to the home page
+				int option = JOptionPane.showConfirmDialog(frame, "Are you sure you would like to go back? This Current Workout will be Lost...", "Cancel?", JOptionPane.YES_NO_OPTION);
+		        if (option == JOptionPane.YES_OPTION) { //user chooses yes
+		        	//go back to home page
 		        	Home home = new Home();
 					home.frame.setVisible(true);
 					frame.setVisible(false);
@@ -127,54 +171,14 @@ public class Page3 {
 		backButton.setBounds(10, 10, 75, 35);
 		frame.getContentPane().add(backButton);
 		
+		//creates the done button
 		JButton doneButton = new JButton("Done");
 		doneButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				boolean valid = true;
-				String invalidInfo ="\n";
-				if (exercises.size()==0) {
-					valid = false;
-					invalidInfo = "|No Exercises|";
-				}
-				for (int i=0;i<exercises.size();i++) {
-					Exercise currentExercise = exercises.get(i);
-					String exerciseName = currentExercise.nameField.getText();
-					if (exerciseName.equals("")) {
-						valid = false; 
-						invalidInfo = invalidInfo+ "|Exercise #"+currentExercise.exerciseNumber+" Name Invalid|\n";
-					}
-					for (int k=0;k<currentExercise.sets.size();k++) {
-						Set currentSet = currentExercise.sets.get(k);
-						String reps = currentSet.repsField.getText();
-						String weight = currentSet.weightField.getText();
-						if (reps.equals("")) {
-							valid = false; 
-							invalidInfo = invalidInfo+ "|Exercise #"+currentExercise.exerciseNumber+" Set #"+currentSet.setNumber+" Reps Invalid|\n";
-						}
-						if (weight.equals("")) {
-							valid = false; 
-							invalidInfo = invalidInfo+ "|Exercise #"+currentExercise.exerciseNumber+" Set #"+currentSet.setNumber+" Weight Invalid|\n";
-						}
-						try {
-					        int d = Integer.parseInt(reps);
-					    } catch (NumberFormatException nfe) {
-					    	if (!reps.equals("")) {
-					    		valid = false; 
-						    	invalidInfo = invalidInfo+ "|Exercise #"+currentExercise.exerciseNumber+" Set #"+currentSet.setNumber+" Reps must be an Integer|\n";
-					    	}
-					    }
-						try {
-					        double d = Double.parseDouble(weight);
-					    } catch (NumberFormatException nfe) {
-					    	if (!weight.equals("")) {
-					    		valid = false; 
-						    	invalidInfo = invalidInfo+ "|Exercise #"+currentExercise.exerciseNumber+" Set #"+currentSet.setNumber+" Weight must be a double|\n";
-					    	}
-					    }
-					}
-				}
 				
-				if (valid) {
+				String invalidInfo = isValid();
+				if (!invalidInfo.equals("\n")) JOptionPane.showMessageDialog(frame, "Invalid Input:\n"+invalidInfo);
+				else {
 					String url = "jdbc:mysql://localhost:3306/Flexx" ;
 					String user = "root" ;
 					String sizeQuery = "SELECT COUNT(*) FROM exerciselogs";
@@ -230,9 +234,6 @@ public class Page3 {
 					Home home = new Home();
 					home.frame.setVisible(true);
 					frame.setVisible(false);
-				}
-				else {
-					JOptionPane.showMessageDialog(frame, "Invalid Input:\n"+invalidInfo);
 				}
 				
 			}
