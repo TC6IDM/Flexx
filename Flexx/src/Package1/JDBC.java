aeeb7beb6687d51ec40cb02276766bb1126da327
AvaYunus <avayunus02@gmail.com>
2023-03-09 16:02:46 -0500

    mySQL Login and Sign up
    Created a functional Login and Sign up system using mySQL.

--- Flexx/src/Package1/JDBC.java
+++ Flexx/src/Package1/JDBC.java
@@ -1,5 +1,6 @@
 package Package1;
 import java.sql.*;
+
 public class JDBC{
 	private static String password = "MyN3wP4ssw0rd";// replace ... with your password
 	public static void main(String[] args) {}
@@ -24,11 +25,13 @@ public class JDBC{
 		}
 		System.out.println("database created");
 	}
+	
+	
 	public static void createTables() {
 		String url = "jdbc:mysql://localhost:3306/Flexx" ;
 		String user = "root" ;
 		
-		String query = "CREATE TABLE userInfo( username VARCHAR(255) NOT NULL, password VARCHAR(255) NOT NULL);" ;
+		String query = "CREATE TABLE IF NOT EXISTS userInfo( Uname VARCHAR(255) NOT NULL, Upassword VARCHAR(255) NOT NULL);" ;
 		
 		try {
 			// create connection
@@ -46,4 +49,56 @@ public class JDBC{
 		}
 		System.out.println("table 1 created");
 	}
+	
+	
+	public static void insertUser(String Uname, String Upassword) {
+		String url = "jdbc:mysql://localhost:3306/Flexx" ;
+		String user = "root" ;
+		String query = "INSERT INTO userInfo (username, password) VALUES (?, ?)";
+		
+		try {
+			// create connection
+			Connection con = DriverManager.getConnection (url,user,password);
+			
+			 // create statement
+			PreparedStatement pstmt = con.prepareStatement(query);
+			pstmt.setString(1, Uname);
+			pstmt.setString(2, Upassword);
+			
+			 // execute statement
+			pstmt.executeUpdate();
+			
+			 // close resources
+		    pstmt.close();
+		    con.close();
+		} catch (SQLException e) {
+		}
+	}
+	
+	public static boolean checkUser(String Uname, String Upassword) throws SQLException {
+	    String url = "jdbc:mysql://localhost:3306/Flexx";
+	    String user = "root";
+	    String query = "SELECT * FROM userinfo WHERE username = ? AND password = ?";
+	    boolean found = false;
+
+	    // create connection
+	    Connection conn = DriverManager.getConnection(url, user, password);
+
+	    // create prepared statement
+	    PreparedStatement stmt = conn.prepareStatement(query);
+	    stmt.setString(1, Uname);
+	    stmt.setString(2, Upassword);
+
+	    // execute query and check result
+	    ResultSet rs = stmt.executeQuery();
+	    found = rs.next();
+
+	    // close resources
+	    rs.close();
+	    stmt.close();
+	    conn.close();
+
+	    return found;
+	}
+	
 }
--- Flexx/src/Package1/Login.java
+++ Flexx/src/Package1/Login.java
@@ -8,12 +8,7 @@ import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.sql.*;
 import java.awt.event.ActionEvent;
 import javax.swing.JPasswordField;
 
@@ -79,39 +74,66 @@ public class Login implements ActionListener{
 		lblUsername.setBounds(51, 58, 140, 35);
 		frame.getContentPane().add(lblUsername);
 				
+//		JButton btnNewButton = new JButton("Login");
+//		btnNewButton.addActionListener(new ActionListener() {
+//			public void actionPerformed(ActionEvent e) {
+//				String username = usernameField.getText();
+//				String password = passwordField.getText();
+//				boolean found = false;
+//				
+//		        try {
+//		            BufferedReader reader = new BufferedReader(new FileReader("users.txt"));
+//		            String line;
+//		            while ((line = reader.readLine()) != null) {
+//		                String[] parts = line.split(",");
+//		                if (parts.length == 2 && parts[0].equals(username) && parts[1].equals(password)) {
+//		                    found = true;
+//		                    break;
+//		                }
+//		            }
+//		            reader.close();
+//		        } catch (IOException ex) {
+//		            ex.printStackTrace();
+//		        }
+//		        
+//		        if (found) {
+//		        	JOptionPane.showMessageDialog(frame, "Welcome");
+//		        	Home home = new Home();
+//					home.frame.setVisible(true);
+//					frame.setVisible(false);
+//					
+//		        } else {
+//					JOptionPane.showMessageDialog(frame, "Invalid Login");
+//				}
+//				
+// 			}
+//		});
+//		btnNewButton.setBounds(176, 189, 85, 21);
+//		frame.getContentPane().add(btnNewButton);
+		
 		JButton btnNewButton = new JButton("Login");
 		btnNewButton.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				String username = usernameField.getText();
-				String password = passwordField.getText();
-				boolean found = false;
-				
-		        try {
-		            BufferedReader reader = new BufferedReader(new FileReader("users.txt"));
-		            String line;
-		            while ((line = reader.readLine()) != null) {
-		                String[] parts = line.split(",");
-		                if (parts.length == 2 && parts[0].equals(username) && parts[1].equals(password)) {
-		                    found = true;
-		                    break;
-		                }
-		            }
-		            reader.close();
-		        } catch (IOException ex) {
-		            ex.printStackTrace();
-		        }
+		    public void actionPerformed(ActionEvent e) {
+		        String username = usernameField.getText();
+		        String password = passwordField.getText();
+		        boolean found = false;
 		        
+		        try {
+		        	JDBC.createTables();
+		        	found = JDBC.checkUser(username, password);
+		        	
+		        } catch (SQLException ex) {
+		        }
+		            
 		        if (found) {
-		        	JOptionPane.showMessageDialog(frame, "Welcome");
-		        	Home home = new Home();
-					home.frame.setVisible(true);
-					frame.setVisible(false);
-					
+		            JOptionPane.showMessageDialog(frame, "Welcome");
+		            Home home = new Home();
+		            home.frame.setVisible(true);
+		            frame.setVisible(false);
 		        } else {
-					JOptionPane.showMessageDialog(frame, "Invalid Login");
-				}
-				
- 			}
+		            JOptionPane.showMessageDialog(frame, "Invalid Login");
+		        }
+		    }
 		});
 		btnNewButton.setBounds(176, 189, 85, 21);
 		frame.getContentPane().add(btnNewButton);
@@ -126,69 +148,34 @@ public class Login implements ActionListener{
 		btnClearButton.setBounds(47, 185, 117, 29);
 		frame.getContentPane().add(btnClearButton);
 		
-//		JButton btnNewButton_1 = new JButton("Sign up");
-//		btnNewButton_1.setBounds(273, 185, 117, 29);
-//		frame.getContentPane().add(btnNewButton_1);
-//		btnNewButton_1.addActionListener(new ActionListener() {
-//		    public void actionPerformed(ActionEvent e) {
-//		        JTextField usernameField = new JTextField();
-//		        JTextField passwordField = new JPasswordField();
-//		        Object[] fields = {
-//		            "Username:", usernameField,
-//		            "Password:", passwordField
-//		        };
-//		        int option = JOptionPane.showConfirmDialog(frame, fields, "Sign up", JOptionPane.OK_CANCEL_OPTION);
-//		        if (option == JOptionPane.OK_OPTION) {
-//		            String username = usernameField.getText();
-//		            String password = passwordField.getText();
-//		            // save the username and password to a database or file
-//		            try {
-//		            	File file = new File("users.txt");
-//		            	if(!file.exists()) {
-//		            		file.createNewFile();
-//		            	}
-//		                BufferedWriter writer = new BufferedWriter(new FileWriter(file, true));
-//		                writer.write(username + "," + password + "\n");
-//		                writer.close();
-//		            JOptionPane.showMessageDialog(frame, "You have successfully signed up!");
-//		            } catch (IOException ex) {
-//		                ex.printStackTrace();
-//		            }
-//		        }
-//		    }
-//		});
-		
 		JButton btnNewButton_1 = new JButton("Sign up");
 		btnNewButton_1.setBounds(273, 185, 117, 29);
 		frame.getContentPane().add(btnNewButton_1);
 		btnNewButton_1.addActionListener(new ActionListener() {
-		    public void actionPerformed(ActionEvent e) {
-		        JTextField usernameField = new JTextField();
-		        JTextField passwordField = new JPasswordField();
-		        Object[] fields = {
-		            "Username:", usernameField,
-		            "Password:", passwordField
-		        };
-		        int option = JOptionPane.showConfirmDialog(frame, fields, "Sign up", JOptionPane.OK_CANCEL_OPTION);
-		        if (option == JOptionPane.OK_OPTION) {
-		            String username = usernameField.getText();
-		            String password = passwordField.getText();
-		            // save the username and password to a database or file
-		            try {
-		            	File file = new File("users.txt");
-		            	if(!file.exists()) {
-		            		file.createNewFile();
-		            	}
-		                BufferedWriter writer = new BufferedWriter(new FileWriter(file, true));
-		                writer.write(username + "," + password + "\n");
-		                writer.close();
-		            JOptionPane.showMessageDialog(frame, "You have successfully signed up!");
-		            } catch (IOException ex) {
-		                ex.printStackTrace();
-		            }
-		        }
-		    }
-		});
+			  public void actionPerformed(ActionEvent e) {
+			    JTextField usernameField = new JTextField();
+			    JTextField passwordField = new JPasswordField();
+			    Object[] fields = {
+			      "Username:", usernameField,
+			      "Password:", passwordField
+			    };
+			    int option = JOptionPane.showConfirmDialog(frame, fields, "Sign up", JOptionPane.OK_CANCEL_OPTION);
+			    
+			    if (option == JOptionPane.OK_OPTION) {
+			      String username = usernameField.getText();
+			      String password = passwordField.getText();
+			      
+			      try {
+			    	  JDBC.insertUser(username, password);
+			    	  JOptionPane.showMessageDialog(frame,  "You have successfully signed up!"); 
+			      } catch (Exception ex) {
+			    	  JOptionPane.showMessageDialog(frame, "An error occurred while signing up.");
+			          ex.printStackTrace();
+			      }
+			    }
+			  }
+			});
+	
 	}
 	
 	public JTextField getUsernameField() {
