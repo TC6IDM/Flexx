1ae1be8b21fd794c88a35f46f4ed464cf79f38e4
TC6IDM <TCAND6@GMAIL.COM>
2023-03-20 13:22:34 -0400

    finding all unique exercises

--- Flexx/src/Andrew/Page2.java
+++ Flexx/src/Andrew/Page2.java
@@ -18,12 +18,14 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JScrollPane;
 import javax.swing.JTextField;
 
 import Ava.*;
 import otherUtil.Flexx;
+import otherUtil.JDBC;
 
 import java.awt.Color;
 import javax.swing.SwingConstants;
@@ -59,44 +61,51 @@ public class Page2 {
 	 * @wbp.parser.entryPoint
 	 */
 	private void initialize() {
-		int Frame_Left = 100;
-		int Frame_Width = 450;
-		int Frame_Top = 100;
-		int Frame_Height = 600;
+		//creates the frame
+		Page3 page3 = new Page3();
+		int Frame_Left = page3.Frame_Left;
+		int Frame_Top = page3.Frame_Top;
+		int Frame_Width = page3.Frame_Width;
+		int Frame_Height = page3.Frame_Height;
+		int Frame_ActualWidth = page3.Frame_ActualWidth;
+		
 		frame = new JFrame();
 		frame.getContentPane().setBackground(new Color(77, 77, 77));
 		frame.setBounds(Frame_Left, Frame_Top, Frame_Width, Frame_Height);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
 		frame.getContentPane().setLayout(null);
-		
-//		JLabel lblNewLabel = new JLabel("PAGE 2");
-//		lblNewLabel.setBounds(132, 10, 172, 35);
-//		lblNewLabel.setFont(new Font("Berlin Sans FB", Font.PLAIN, 31));
-//		frame.getContentPane().add(lblNewLabel);
-		
-		int Frame_ActualWidth = Frame_Width - 14;
-		int buttonWidth = 70;
-		int buttonDistanceFromSides = 10;
-		int buttonHeight = 30;
-		JButton btnNewButton = new JButton("Back");
-		btnNewButton.setBounds(buttonDistanceFromSides, buttonDistanceFromSides, buttonWidth, buttonHeight);
-		btnNewButton.addActionListener(new ActionListener() {
+				
+				
+				
+		//creates the back button
+		int buttonWidth = 100;
+		int buttonDistanceFromSides = 0;
+		int buttonHeight = 50;
+		JButton backButton = new JButton("");
+		backButton.setHorizontalAlignment(SwingConstants.LEFT);
+		backButton.setIcon(new ImageIcon(Page3.class.getResource("/Andrew/x-mark-32.png")));
+		backButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				Home home = new Home();
-				home.frame.setVisible(true);
-				frame.setVisible(false);
+		        //creates a pop up confirmation for going back to the home page
+				int option = JOptionPane.showConfirmDialog(frame, "Are you sure you would like to go back? This Current Workout will be Lost...", "Cancel?", JOptionPane.YES_NO_OPTION);
+		        if (option == JOptionPane.YES_OPTION) { //user chooses yes
+		        	//go back to home page
+		        	Home home = new Home();
+					home.frame.setVisible(true);
+					frame.setVisible(false);
+		        }
 			}
 		});
-		frame.getContentPane().add(btnNewButton);
-		
-		JButton doneButton = new JButton("Done");
-		doneButton.setBounds(Frame_ActualWidth-buttonWidth-buttonDistanceFromSides, buttonDistanceFromSides, buttonWidth, buttonHeight);
-		frame.getContentPane().add(doneButton);
-		
-		
+		backButton.setOpaque(false);
+		backButton.setContentAreaFilled(false);
+		backButton.setBorderPainted(false);
+		backButton.setBounds(buttonDistanceFromSides, buttonDistanceFromSides, buttonWidth, buttonHeight);
+		frame.getContentPane().add(backButton);
+				
+		//creates the Track work-out Title
 		int topLabelHeight = 50;
-		JLabel lblTrackWorkout = new JLabel("Track Workout");
+		JLabel lblTrackWorkout = new JLabel("Exercise View");
 		lblTrackWorkout.setVerticalAlignment(SwingConstants.CENTER);
 		lblTrackWorkout.setOpaque(true);
 		lblTrackWorkout.setHorizontalAlignment(SwingConstants.CENTER);
@@ -106,91 +115,19 @@ public class Page2 {
 		lblTrackWorkout.setBounds(0, 0, Frame_ActualWidth, topLabelHeight);
 		frame.getContentPane().add(lblTrackWorkout);
 		
-		int distanceFromTopLabel = 5;
-		int topLabelHeightWithPadding = topLabelHeight + distanceFromTopLabel;
-		int fieldHeight = 20;
-		int nameFieldLength =125;
-		
-		JTextField nameField = new JTextField();
-		nameField.setBounds(Frame_ActualWidth/2 - nameFieldLength/2, topLabelHeightWithPadding, nameFieldLength, fieldHeight);
-		frame.getContentPane().add(nameField);
-		nameField.setColumns(10);
-		
-		int nameLabelWidth = 90;
-		JLabel nameLabel = new JLabel("Exercise Name:");
-		nameLabel.setHorizontalAlignment(SwingConstants.CENTER);
-		nameLabel.setForeground(new Color(255, 255, 255));
-		nameLabel.setBounds(nameField.getX()-nameLabelWidth, topLabelHeightWithPadding, nameLabelWidth, fieldHeight);
-		frame.getContentPane().add(nameLabel);
-		
-		int numberLabelWidth = 18;
-		JLabel exerciseNumberLabel = new JLabel("1");
-		exerciseNumberLabel.setHorizontalAlignment(SwingConstants.CENTER);
-		exerciseNumberLabel.setForeground(Color.WHITE);
-		exerciseNumberLabel.setBounds(nameLabel.getX()-numberLabelWidth, topLabelHeightWithPadding, numberLabelWidth, fieldHeight);
-		frame.getContentPane().add(exerciseNumberLabel);
-		
-		int setRepWeightLabelHeight = exerciseNumberLabel.getY() + exerciseNumberLabel.getHeight()+5;
-		int setRepWeightLabelWidth = 35;
-		
-		JLabel setCount = new JLabel("Set");
-		setCount.setHorizontalAlignment(SwingConstants.CENTER);
-		setCount.setForeground(new Color(255, 255, 255));
-		setCount.setBounds(Frame_ActualWidth/4 - setRepWeightLabelWidth/2, setRepWeightLabelHeight, setRepWeightLabelWidth, fieldHeight);
-		frame.getContentPane().add(setCount);
-		
-		JLabel repsLabel = new JLabel("Reps");
-		repsLabel.setHorizontalAlignment(SwingConstants.CENTER);
-		repsLabel.setForeground(new Color(255, 255, 255));
-		repsLabel.setBounds(Frame_ActualWidth/2 - setRepWeightLabelWidth/2, setRepWeightLabelHeight, setRepWeightLabelWidth, fieldHeight);
-		frame.getContentPane().add(repsLabel);
-		
-		JLabel weightLabel = new JLabel("Weight");
-		weightLabel.setHorizontalAlignment(SwingConstants.CENTER);
-		weightLabel.setForeground(new Color(255, 255, 255));
-		weightLabel.setBounds(Frame_ActualWidth*3/4 - setRepWeightLabelWidth/2, setRepWeightLabelHeight, setRepWeightLabelWidth, fieldHeight);
-		frame.getContentPane().add(weightLabel);
-		
-		int distanceBetween0thAnd1stSets = 25;
-		int firstSetStartingHeight = setRepWeightLabelHeight+distanceBetween0thAnd1stSets;
-		JLabel lblExercise_1 = new JLabel("1");
-		lblExercise_1.setHorizontalAlignment(SwingConstants.CENTER);
-		lblExercise_1.setForeground(Color.WHITE);
-		lblExercise_1.setBounds(Frame_ActualWidth/4 - numberLabelWidth/2, firstSetStartingHeight, numberLabelWidth, fieldHeight);
-		frame.getContentPane().add(lblExercise_1);
-		
-		int textFieldWidth = 70;
-		//creates the reps field
-		JTextField repsField = new JTextField();
-		repsField.setColumns(10);
-		repsField.setBounds(Frame_ActualWidth/2 - textFieldWidth/2, firstSetStartingHeight, textFieldWidth, fieldHeight);
-		frame.getContentPane().add(repsField);
-		
-		//creates the weight field
-		JTextField weightField = new JTextField();
-		weightField.setColumns(10);
-		weightField.setBounds(Frame_ActualWidth*3/4 - textFieldWidth/2, firstSetStartingHeight, textFieldWidth, fieldHeight);
-		frame.getContentPane().add(weightField);
-		
-		int addSetButton_width = 80;
-		int addSetButton_height = 13;
-		int addSetButtonY = repsField.getY()+repsField.getHeight()+5;
-		JButton addSetButton = new JButton("Add set");
-		addSetButton.setBounds(Frame_ActualWidth/2 - addSetButton_width/2, addSetButtonY, addSetButton_width, addSetButton_height);
-		frame.getContentPane().add(addSetButton);		
-		
-		
-		int newExerciseButton_width = 150;
-		int newExerciseButton_height = 35;
-		int NewExerciseButtonY = addSetButton.getY()+addSetButton.getHeight()+5;
-		JButton newExerciseButton = new JButton("New Exercise");
-		newExerciseButton.setBounds(Frame_ActualWidth/2 - newExerciseButton_width/2, NewExerciseButtonY, newExerciseButton_width, newExerciseButton_height);
-		frame.getContentPane().add(newExerciseButton);
-		
-		
-		
-		
-		
+		String exercisesQuery = "SELECT DISTINCT ExerciseName FROM exerciselogs";
+		try {
+			Connection con = DriverManager.getConnection (JDBC.databaseURL,JDBC.user,JDBC.password);
+			Statement statement = con.createStatement();
+			ResultSet rs = statement.executeQuery(exercisesQuery);
+			int i=0;
+			while (rs.next()) {
+				System.out.println(rs.getString(1));
+				i++;
+			}
+		} catch (SQLException err) {
+			err.printStackTrace();
+		}
 		
 
 	}
