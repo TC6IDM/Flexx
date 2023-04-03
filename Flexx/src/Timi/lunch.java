0c3156c2b9125311de394f9893cea8f28f232a17
timmyola 218863761 <122815796+timmyola@users.noreply.github.com>
2023-04-03 02:28:03 -0400

    changing all food templates
    changing all the meal templates to match general

--- Flexx/src/Timi/lunch.java
+++ Flexx/src/Timi/lunch.java
@@ -1,16 +1,27 @@
 package Timi;
 
 
+import java.awt.Color;
+import java.awt.Component;
 import java.awt.EventQueue;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Statement;
 
-import javax.swing.JFrame;
 import javax.swing.JButton;
+import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JTextField;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
 
 import otherUtil.*;
+
 public class lunch {
 
 	public JFrame frame;
@@ -22,6 +33,9 @@ public class lunch {
 	private JTextField textField_5;
 	private JTextField textField_6;
 	private JTextField textField_7;
+	private String textFieldValue = "";
+	private JLabel lblNewLabel_2;
+
 
 	/**
 	 * Launch the application.
@@ -31,7 +45,8 @@ public class lunch {
 			public void run() {
 				try {
 					Flexx.main(args);
-				} catch (Exception e) {
+				} 
+				catch (Exception e) {
 					e.printStackTrace();
 				}
 			}
@@ -40,6 +55,7 @@ public class lunch {
 
 	/**
 	 * Create the application.
+	 * @return 
 	 */
 	public lunch() {
 		initialize();
@@ -50,16 +66,19 @@ public class lunch {
 	 * @wbp.parser.entryPoint
 	 */
 	private void initialize() {
-		
+
 		frame = new JFrame();
 		frame.setBounds(100, 100, 450, 300);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
-		
+
 		JLabel lblNewLabel = new JLabel("LUNCH");
+		lblNewLabel.setForeground(Color.WHITE);
 		lblNewLabel.setBounds(171, 6, 81, 16);
 		frame.getContentPane().add(lblNewLabel);
-		
+		frame.getContentPane().setBackground(new Color(77, 77, 77));
+
+
 		JButton btnNewButton = new JButton("BACK");
 		btnNewButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -70,62 +89,219 @@ public class lunch {
 		});
 		btnNewButton.setBounds(0, 1, 89, 29);
 		frame.getContentPane().add(btnNewButton);
-		
-		textField = new JTextField();
-		textField.setBounds(10, 53, 130, 26);
-		frame.getContentPane().add(textField);
-		textField.setColumns(10);
+
 		
 		textField_1 = new JTextField();
-		textField_1.setBounds(10, 105, 130, 26);
+		textField_1.setBounds(41, 105, 130, 26);
 		frame.getContentPane().add(textField_1);
 		textField_1.setColumns(10);
-		
+
 		textField_2 = new JTextField();
-		textField_2.setBounds(10, 157, 130, 26);
+		textField_2.setBounds(41, 157, 130, 26);
 		frame.getContentPane().add(textField_2);
 		textField_2.setColumns(10);
-		
+
 		textField_3 = new JTextField();
-		textField_3.setBounds(10, 214, 130, 26);
+		textField_3.setBounds(41, 214, 130, 26);
 		frame.getContentPane().add(textField_3);
 		textField_3.setColumns(10);
-		
+
 		textField_4 = new JTextField();
-		textField_4.setBounds(171, 53, 130, 26);
+		textField_4.setBounds(41, 57, 130, 26);
 		frame.getContentPane().add(textField_4);
 		textField_4.setColumns(10);
-		
+
 		textField_5 = new JTextField();
-		textField_5.setBounds(171, 105, 130, 26);
+		textField_5.setBounds(220, 105, 130, 26);
 		frame.getContentPane().add(textField_5);
 		textField_5.setColumns(10);
-		
+
 		textField_6 = new JTextField();
-		textField_6.setBounds(171, 157, 130, 26);
+		textField_6.setBounds(220, 157, 130, 26);
 		frame.getContentPane().add(textField_6);
 		textField_6.setColumns(10);
-		
+
 		textField_7 = new JTextField();
-		textField_7.setBounds(171, 214, 130, 26);
+		textField_7.setBounds(220, 57, 130, 26);
 		frame.getContentPane().add(textField_7);
 		textField_7.setColumns(10);
 		
-		JLabel lblNewLabel_1 = new JLabel("total cal");
-		lblNewLabel_1.setBounds(347, 58, 61, 16);
+		JButton btnNewButton_1 = new JButton("Save");
+		btnNewButton_1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+				try {
+                    // Establish a connection to the database
+                    Connection con = DriverManager.getConnection(JDBC.databaseURL, JDBC.user, JDBC.password);
+
+                    // Create a table to store the data
+            		String query = "INSERT INTO LUNCH (calories) VALUES ('" + textFieldValue + "')";
+
+            		 Statement stmt = con.createStatement();
+                     stmt.executeUpdate(query);
+
+                     // Insert the data into the table
+                     String insertSql = "INSERT INTO LUNCH (calories) VALUES (?)";
+                     PreparedStatement pstmt = con.prepareStatement(insertSql);
+                     Component[] components = frame.getContentPane().getComponents();
+                     boolean dataInserted = false;
+                     for (Component component : components) {
+                         if (component instanceof JTextField) {
+                             String calories = ((JTextField) component).getText();
+                             pstmt.setString(1, calories);
+                             int rows = pstmt.executeUpdate();
+                             if (rows > 0) {
+                                 dataInserted = true;
+                             }
+                         }
+                     }
+
+                     // Close the connection
+                     pstmt.close();
+                     stmt.close();
+                     con.close();
+                     // Show a message to indicate whether the data has been saved
+                     if (dataInserted) {
+                         JOptionPane.showMessageDialog(frame, "Data has been saved to the database.");
+                     } else {
+                         JOptionPane.showMessageDialog(frame, "No data was inserted into the database.");
+                     }
+                 } catch (SQLException ex) {
+                     JOptionPane.showMessageDialog(frame, "Error: " + ex.getMessage());
+                 }
+             }
+         });
+		
+		btnNewButton_1.setBounds(384, 237, 60, 29);
+		frame.getContentPane().add(btnNewButton_1);
+		
+		JLabel lblNewLabel_1 = new JLabel("Total Cal:");
+		lblNewLabel_1.setForeground(Color.WHITE);
+		lblNewLabel_1.setBounds(220, 219, 61, 16);
 		frame.getContentPane().add(lblNewLabel_1);
 		
-		JLabel lblNewLabel_2 = new JLabel("total cal");
-		lblNewLabel_2.setBounds(347, 110, 61, 16);
+		lblNewLabel_2 = new JLabel("New label");
+		lblNewLabel_2.setForeground(Color.ORANGE);
+		lblNewLabel_2.setBounds(289, 219, 61, 16);
 		frame.getContentPane().add(lblNewLabel_2);
-		
-		JLabel lblNewLabel_3 = new JLabel("total cal");
-		lblNewLabel_3.setBounds(347, 162, 61, 16);
-		frame.getContentPane().add(lblNewLabel_3);
-		
-		JLabel lblNewLabel_4 = new JLabel("total cal");
-		lblNewLabel_4.setBounds(347, 219, 61, 16);
-		frame.getContentPane().add(lblNewLabel_4);
-		
+
+		   // Add document listeners to the text fields to update their values
+	    // whenever the user types in them
+	 
+	    textField_1.getDocument().addDocumentListener(new DocumentListener() {
+	        public void changedUpdate(DocumentEvent e) {
+	            updateTextField_1Value();
+	        }
+	        public void removeUpdate(DocumentEvent e) {
+	            updateTextField_1Value();
+	        }
+	        public void insertUpdate(DocumentEvent e) {
+	            updateTextField_1Value();
+	        }
+	    });
+	    textField_2.getDocument().addDocumentListener(new DocumentListener() {
+	        public void changedUpdate(DocumentEvent e) {
+	            updateTextField_2Value();
+	        }
+	        public void removeUpdate(DocumentEvent e) {
+	            updateTextField_2Value();
+	        }
+	        public void insertUpdate(DocumentEvent e) {
+	            updateTextField_2Value();
+	        }
+	    });
+	    textField_3.getDocument().addDocumentListener(new DocumentListener() {
+	        public void changedUpdate(DocumentEvent e) {
+	            updateTextField_3Value();
+	        }
+	        public void removeUpdate(DocumentEvent e) {
+	            updateTextField_3Value();
+	        }
+	        public void insertUpdate(DocumentEvent e) {
+	            updateTextField_3Value();
+	        }
+	    });
+	    textField_4.getDocument().addDocumentListener(new DocumentListener() {
+	        public void changedUpdate(DocumentEvent e) {
+	            updateTextField_4Value();
+	        }
+	        public void removeUpdate(DocumentEvent e) {
+	            updateTextField_4Value();
+	        }
+	        public void insertUpdate(DocumentEvent e) {
+	            updateTextField_4Value();
+	        }
+	    });
+	    textField_5.getDocument().addDocumentListener(new DocumentListener() {
+	        public void changedUpdate(DocumentEvent e) {
+	            updateTextField_5Value();
+	        }
+	        public void removeUpdate(DocumentEvent e) {
+	            updateTextField_5Value();
+	        }
+	        public void insertUpdate(DocumentEvent e) {
+	            updateTextField_5Value();
+	        }
+	    });
+	    textField_6.getDocument().addDocumentListener(new DocumentListener() {
+	        public void changedUpdate(DocumentEvent e) {
+	            updateTextField_6Value();
+	        }
+	        public void removeUpdate(DocumentEvent e) {
+	            updateTextField_6Value();
+	        }
+	        public void insertUpdate(DocumentEvent e) {
+	            updateTextField_6Value();
+	        }
+	    }
+	    );
+	    textField_7.getDocument().addDocumentListener(new DocumentListener() {
+	        public void changedUpdate(DocumentEvent e) {
+	            updateTextField_7Value();
+	        }
+	        public void removeUpdate(DocumentEvent e) {
+	            updateTextField_7Value();
+	        }
+	        public void insertUpdate(DocumentEvent e) {
+	            updateTextField_7Value();
+	        }
+	    }
+	    );
+	  
+	}
+	
+//	public String getTextFieldValue() {
+//	    return textFieldValue;
+//	}
+
+	
+	private void updateTextField_1Value() {
+	    textFieldValue = textField_1.getText();
+	}
+	private void updateTextField_2Value() {
+	    textFieldValue = textField_2.getText();
+	}
+	private void updateTextField_3Value() {
+	    textFieldValue = textField_3.getText();
+	}
+	private void updateTextField_4Value() {
+	    textFieldValue = textField_4.getText();
+	}
+	private void updateTextField_5Value() {
+	    textFieldValue = textField_5.getText();
+	}
+	private void updateTextField_6Value() {
+	    textFieldValue = textField_6.getText();
+	}
+	private void updateTextField_7Value() {
+	    textFieldValue = textField_7.getText();
+	}
+
+	public JTextField getTextField() {
+		return textField;
+	}
+
+	public void setTextField(JTextField textField) {
+		this.textField = textField;
 	}
 }
